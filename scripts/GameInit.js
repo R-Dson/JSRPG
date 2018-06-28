4ffb68a225813700d49c8b97574792914a45aaf2
RobinDson <robindavidsson@outlook.com>
2018-06-28 07:55:52 +0200

    Lots of great new things

--- scripts/GameInit.js
+++ scripts/GameInit.js
@@ -5,7 +5,13 @@ var keyState = {};
 var npcs = [];
 var left = 37, up = 38, right = 39, down = 40;
 var isfightning = false;
-
+var drawingTool = new drawing();
+var gameEvent = new gameEvents();
+var itemsImage = new Image();
+var Jsoninfo;
+var totalattack;
+var playerdamage;
+var npcdamage;
 
 window.onload = function () {
     startGame();
@@ -13,7 +19,7 @@ window.onload = function () {
 
 function startGame() {
     loadGame();
-
+    importItemImages();
     runGame();
 }
 
@@ -23,67 +29,173 @@ function loadGame() {
         start: function () {
             this.context = this.gameCanvas.getContext("2d");
             this.frameNumber = 0;
-            this.tick = setInterval(gameUpdate, 16)
+            this.tick = setInterval(gameUpdate, 1000 / 144);
+            this.fighttick = setInterval(fight, 16)
         },
         clear: function () {
-            this.context.clearRect(0, 0, this.gameCanvas.width, this.gameCanvas.height)
+            this.context.clearRect(0, 0, this.gameCanvas.width, this.gameCanvas.height);
         }
     };
+
 }
 
 function runGame() {
-    var loadedEquipment = "";
+    var loadedItems = "";
+    fetch('../resources/GameJSON.json').then(function (Response) {
+        return Response.json();
+    }).then(function (json) {
+        // var item1 = json.Equipment.helm;
+        // var item2 = json.Equipment.chest;
+        // var item3 = json.Equipment.pants;
+        // var item4 = json.Equipment.weapon;
+        // var item5 = json.Equipment.offhand;
 
-    playerInfo = new character(canvas.gameCanvas.clientWidth, canvas.gameCanvas.clientHeight, 100, 100, 0, 100, loadedEquipment, 10, "red");
-    for (var i = 0; i < Math.round(Math.random() * 25); i++) {
-        var randomwidth = Math.round(Math.random(0) * 400);
-        var randomheight = Math.round(Math.random(0) * 400);
-        var randomhp = Math.round(randomheight * randomwidth * Math.random() * 0.1)/100;
-        npcs.push(new character(Math.round(Math.random(0) * canvas.gameCanvas.width), Math.round(Math.random(0) * canvas.gameCanvas.height), randomwidth, randomheight, 0, randomhp, "", 10, "blue"));
-    }
+        var items = [json.Equipment.helm, json.Equipment.chest, json.Equipment.pants, json.Equipment.weapon, json.Equipment.offhand];
+        var itemsInv = [json.Inventory.slot0, json.Inventory.slot1, json.Inventory.slot2, json.Inventory.slot3, json.Inventory.slot4, json.Inventory.slot5, json.Inventory.slot6, json.Inventory.slot7, json.Inventory.slot8, json.Inventory.slot9];
+        playerInfo = new character("Player", canvas.gameCanvas.clientWidth, canvas.gameCanvas.clientHeight, 100, 100, json.PlayerStats.Experience, json.PlayerStats.healthPoints, items, itemsInv, 4, "red", 10, 10, 10, 1);
+    });
+
+    generateNPC();
     canvas.start();
 }
 
-function character(x, y, width, height, experience, healthPoints, equipmentJson, walkSpeed, color) {
+function drawing() {
+    this.updateplayerdetails = function () {
+        var ctx = canvas.context;
+
+        var fontsize = 25;
+        ctx.font = fontsize + "px Georgia";
+        ctx.fillStyle = "white";
+
+        //Display new lines of info below, each new row is a multiplier of fontsize in y-coordiante
+        ctx.fillText("Player details: ", 3, fontsize);
+        ctx.fillText("hp: " + playerInfo.healthPoints + " / " + playerInfo.displayhealth, 3, fontsize * 2);
+    }
+
+    this.displaydamage = function (displaydamage, x, y, width, height) {
+        var ctx = canvas.context;
+        ctx.font = "20px Georgia";
+        ctx.fillStyle = "white";
+        ctx.fillText(displaydamage, x + width * .5, y + height / 2);
+    }
+
+    this.displayhealthonscreen = function (totalhp, hpleft, x, y) {
+        var ctx = canvas.context;
+        ctx.font = "25px Georgia";
+        ctx.fillStyle = "white";
+        ctx.fillText(Math.round(hpleft * 100) / 100 + " / " + totalhp, x, y - 5);
+    }
+
+    this.drawInterface = function () {
+        var ctx = canvas.context;
+        ctx.fillStyle = "#201F1D";
+        height = 64;
+        ctx.fillRect(canvas.gameCanvas.width / 4, canvas.gameCanvas.height - height, canvas.gameCanvas.width / 2, height);
+    }
+
+    this.drawitem = function(item, index) {
+        var x = canvas.gameCanvas.width / 4;
+        var y = canvas.gameCanvas.height - height;
+        var ctx = canvas.context;
+        ctx.drawImage(itemsImage, 0, 0, 16, 16, x*item.multiplierx, y*item.multipliery, 64, 64);
+        if(item.stackable == 1 && item != null){
+            ctx.font = "25px Georgia";
+            ctx.fillStyle = "white";
+            ctx.fillText(item.Amount, x*(index+1) + 4, y + 19);
+        }
+        
+    }
+
+}
+
+function gameEvents() {
+    this.ondeath = function (deadcharacter) {
+        deadcharacter.healthPoints = 0;
+        if (fightningNPC == deadcharacter) {
+            var i = npcs.indexOf(fightningNPC);
+            if (i >= 0) {
+                playerInfo.experience += fightningNPC.experience;
+
+                npcs.splice(i, 1);
+                isfightning = false;
+            }
+        }
+        else {
+
+        }
+    }
+}
+
+function Item(itemStatsJSON) {
+    this.attack = itemStatsJSON.Attack;
+    this.defence = itemStatsJSON.Defence;
+    this.strength = itemStatsJSON.Strength;
+    this.Name = itemStatsJSON.Name;
+    this.lvlreq = itemStatsJSON.LevelRequirement;
+    this.multiplier = itemStatsJSON.multiplier;
+}
+
+function playerItems(items) {
+    this.helm = new Item(items[0]);
+    this.chest = new Item(items[1]);
+    this.pants = new Item(items[2]);
+    this.weapon = new Item(items[3]);
+    this.offhand = new Item(items[4]);
+}
+
+function playerInventory(items){
+    
+}
+
+function character(type, x, y, width, height, experience, healthPoints, equipment, inventory, walkSpeed, color, attack = 10, defence = 10, strength = 10, level = 1) {
+
     this.x = x;
     this.y = y;
     this.experience = experience;
     this.healthPoints = healthPoints;
     this.displayhealth = healthPoints;
-    this.equipmentJson = equipmentJson;
+    this.inventory = inventory;
+
+    if (type == "Player") {
+        // this.equipment = new playerItems(equipment);
+        this.attackPower = attack;
+        this.defencePower = defence;
+        this.strengthPower = strength;
+        for (i = 0; i < equipment.length; i++) {
+            var app = equipment[i].Attack;
+            var dpp = equipment[i].Defence;
+            var spp = equipment[i].Defence;
+            this.attackPower += app;
+            this.defencePower += dpp;
+            this.strengthPower += spp;
+        }
+        this.inventoryitems = [];
+        for(i = 0; i < inventory.length; i++){
+            this.inventoryitems.push(inventory[i]);
+        }
+        this.equipmentitems = []
+        for(i = 0; i < equipment.length; i++){
+            this.equipmentitems.push(equipment[i]);
+        }
+        
+    }
+
     this.height = width;
     this.width = height;
     this.walkSpeed = walkSpeed;
-    //this.drawArea = canvas.context;
+    this.currency = 0;
+    this.attack = attack;
+    this.defence = defence;
+    this.attack = attack;
+    this.strength = strength
+    this.level = level;
+
     this.updatecharacter = function () {
         drawArea = canvas.context;
         drawArea.fillStyle = color;
         drawArea.fillRect(this.x, this.y, this.width, this.height);
     }
-
-    this.updateplayerdetails = function () {
-        var ctx = canvas.context;
-        var fontsize = 50;
-        ctx.font = fontsize + "px Georgia";
-        ctx.fillStyle = "white";
-        ctx.fillText("Player details: ", 3, fontsize);
-        ctx.fillText("hp: " + playerInfo.healthPoints + " / " + playerInfo.displayhealth, 3, fontsize * 2)
-    }
-
-    this.displaydamage = function (displaydamage, x, y, width, height) {
-        var ctx = canvas.context;
-        ctx.font = "50px Georgia";
-        ctx.fillStyle = "white";
-        ctx.fillText(displaydamage, x + width * .5, y + height / 2);
-
-    }
-
-    this.displayhealthonscreen = function(totalhp, hpleft, x, y){
-        var ctx = canvas.context;
-        ctx.font = "50px Georgia";
-        ctx.fillStyle = "white";
-        ctx.fillText(hpleft + " / " + totalhp, x, y-5);
-    }
+    //#201F1D
 
     var bottom = canvas.gameCanvas.height - this.height, topAndLeftLimit = 0, rightlimit = canvas.gameCanvas.width - this.width;
 
@@ -98,6 +210,7 @@ function character(x, y, width, height, experience, healthPoints, equipmentJson,
         if (this.x > rightlimit)
             this.x = rightlimit;
     }
+
     this.npclimit = function () {
         if (this.y > bottom) {
             this.y = bottom;
@@ -125,25 +238,6 @@ function character(x, y, width, height, experience, healthPoints, equipmentJson,
         }
         return true;
     }
-
-    this.ondeath = function (deadcharacter) {
-        deadcharacter.healthPoints = 0;
-        if (fightningNPC == deadcharacter) {
-            var i = npcs.indexOf(fightningNPC);
-            if (i >= 0) {
-                playerInfo.experience += fightningNPC.experience;
-
-                npcs.splice(i, 1);
-                isfightning = false;
-            }
-        }
-        else{
-
-        }
-
-    }
-
-
 }
 
 var fightningNPC;
@@ -160,12 +254,9 @@ function gameUpdate() {
                 break;
             }
         }
-        playerInfo.updatecharacter();
-
 
         canvas.clear();
         canvas.frameNumber += 1;
-
         //MOVEMENT LOGIC ####################################################################
 
         if (!isfightning) {
@@ -212,61 +303,73 @@ function gameUpdate() {
                     npcs[i].x -= Math.round(Math.random()) * npcs[i].walkSpeed;
             }
         }
+        playerInfo.updatecharacter();
 
         for (i = 0; i < npcs.length; i++) {
             npcs[i].updatecharacter();
-            npcs[i].displayhealthonscreen(npcs[i].displayhealth, npcs[i].healthPoints, npcs[i].x, npcs[i].y);
+            drawingTool.displayhealthonscreen(npcs[i].displayhealth, npcs[i].healthPoints, npcs[i].x, npcs[i].y);
         }
 
         //triggeres when you collide with an enemy, fighting logic here ##########################################################################
+
+        //fight();
         if (isfightning) {
-            playerFightTick++;
-            npcFightTick++;
-            var playerallowattack = fightT(playerFightTick);
-            var npcAllowAttack = fightT(npcFightTick);
-            if (playerallowattack) {
-                var playerdamage = Math.round(Math.random() * 25);
-                playerInfo.displaydamage(playerdamage, fightningNPC.x, fightningNPC.y, fightningNPC.width, fightningNPC.height);
-                fightningNPC.healthPoints -= playerdamage;
-                playerFightTick = 0;
+            if (playerdamage != null) {
+                drawingTool.displaydamage(playerdamage, fightningNPC.x, fightningNPC.y, fightningNPC.width, fightningNPC.height);
             }
-            if (npcAllowAttack) {
-                var npcdamage = Math.round(Math.random() * 15);
-                fightningNPC.displaydamage(playerInfo.displayhealth, playerInfo.healthPoints, playerInfo.x, playerInfo.y);
-                playerInfo.healthPoints -= npcdamage;
-                npcFightTick = 0;
-            }
-            if (fightningNPC.healthPoints <= 0) {
-                fightningNPC.ondeath(fightningNPC);
-            }
-            if (playerInfo.healthPoints <= 0) {
-                delete window.playerInfo;
-                gameRunState = false;
+            if (npcdamage != null) {
+                drawingTool.displaydamage(playerInfo.displayhealth, playerInfo.healthPoints, playerInfo.x, playerInfo.y);
             }
         }
-        playerInfo.updatecharacter();
-        playerInfo.updateplayerdetails();
-        playerInfo.displayhealthonscreen(playerInfo.displayhealth, playerInfo.healthPoints, playerInfo.x, playerInfo.y);
+
+        updateTable(playerInfo.healthPoints, playerInfo.displayhealth, playerInfo.attackPower, playerInfo.defence, playerInfo.experience, playerInfo.level);
+        
+
+        //Drawing ##########################################################################
+        drawingTool.displayhealthonscreen(playerInfo.displayhealth, playerInfo.healthPoints, playerInfo.x, playerInfo.y);
         playerInfo.limit();
-
+        importItemImages();
+        drawingTool.drawInterface();
+        for(i = 0; i < 5; i++){
+            drawingTool.drawitem(playerInfo.inventoryitems[i], i);
+        }
     }
 }
 
+// function updateInverval(number) {
+//     if ((canvas.frameNumber / number) % 6 == 0)
+//         return true;
+//     return false;
+// }
 
+function fight() {
+    if (isfightning) {
 
-function updateInverval(number) {
-    if ((canvas.frameNumber / number) % 1 == 0)
-        return true;
-    return false;
-}
+        playerFightTick++;
+        npcFightTick++;
 
-function gamestate() {
-    this.pausegame = function () {
-        gameRunState = false;
-    }
+        var playerallowattack = fightT(playerFightTick);
+        var npcAllowAttack = fightT(npcFightTick);
 
-    this.returnGame = function () {
-        gameRunState = true;
+        if (playerallowattack && playerInfo.healthPoints > 0) {
+            playerdamage = Math.round(Math.random() * 2.5 * playerInfo.attackPower - fightningNPC.defence / 10);
+            fightningNPC.healthPoints -= playerdamage;
+            playerFightTick = 0;
+        }
+        if (npcAllowAttack && fightningNPC.healthPoints > 0) {
+            npcdamage = Math.round(Math.random() * 15 - playerInfo.defence/10);
+            playerInfo.healthPoints -= npcdamage;
+            npcFightTick = 0;
+        }
+        if (fightningNPC.healthPoints <= 0) {
+            gameEvent.ondeath(fightningNPC);
+            fightningNPC.healthPoints = 0;
+        }
+        if (playerInfo.healthPoints <= 0) {
+            playerInfo.healthPoints = 0;
+            delete window.playerInfo;
+            gameRunState = false;
+        }
     }
 }
 
@@ -283,8 +386,29 @@ function fightT(tick) {
     return false
 }
 
+function generateNPC() {
+    for (var i = 0; i < Math.round(Math.random() * 25); i++) {
+        var randomwidth = Math.round(Math.random(0) * 400);
+        var randomheight = Math.round(Math.random(0) * 400);
+        var randomhp = Math.round(randomheight * randomwidth * Math.random() * 0.1) / 100;
+        npcs.push(new character("NPC", Math.round(Math.random(0) * canvas.gameCanvas.width), Math.round(Math.random(0) * canvas.gameCanvas.height), randomwidth, randomheight, randomhp, randomhp, "", "", 10, "blue"));
+    }
+}
 
-// Events
+function BuyHPotion(){
+    for(i = 0; i <= playerInfo.inventoryitems.length; i++){
+        if(playerInfo.inventoryitems[i].Name == "Health Potion"){
+            playerInfo.inventoryitems[i].Amount++;
+        }
+    }
+}
+
+function importItemImages() {
+    var ctx = canvas.gameCanvas.getContext("2d");
+    itemsImage.src = "../resources/rpgitems.png";
+}
+
+// Events triggers ###########################################################################################
 document.addEventListener('keydown',
     function (event) {
         keyState[event.keyCode || event.which] = true;
@@ -293,7 +417,14 @@ document.addEventListener('keydown',
 document.addEventListener('keyup',
     function (event) {
         keyState[event.keyCode || event.which] = false;
-    }, gameRunState)
+    }, gameRunState
+);
+
+document.addEventListener('scroll',
+    function () {
+        window.scrollTo(0, 0);
+    }, true
+);
 
 document.addEventListener('touchstart',
     function (event) {
@@ -307,4 +438,60 @@ document.addEventListener('touchstart',
             playerInfo.x += playerInfo.walkSpeed;
         if (eventy > playerInfo.y)
             playerInfo.y += playerInfo.walkSpeed;
-    }, gameRunState)
+    }, gameRunState
+);
+
+//menu scripts goes here ####################################################
+
+function switchTab(event, tabName) {
+    var tabs = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    var tab = document.getElementById(tabName);
+    tab.style.display = "block";
+}
+
+function updateTable(currentHP, totalHp, playerAttack, playerDefence, playerexperience, playerlevel) {
+    var healthPointstd, attack, defence, experience, level, trinventory, trequipment;
+
+    healthPointstd = document.getElementById("HealthPoints");
+    healthPointstd.innerHTML = "Health: " + currentHP + " / " + totalHp;
+
+    attack = document.getElementById("Attack");
+    attack.innerHTML = "Attack: " + playerAttack;
+
+    defence = document.getElementById("Defence");
+    defence.innerHTML = "Defence: " + playerDefence;
+
+    experience = document.getElementById("Experience");
+    experience.innerHTML = "Experience: " + Math.round(playerexperience * 100) / 100;
+
+    level = document.getElementById("Level");
+    level.innerHTML = "Level: " + playerlevel;
+
+    trinventory = document.getElementById("Inventory");
+    
+    for (let i = 0; i < playerInfo.inventoryitems.length; i++) {
+        var name = playerInfo.inventoryitems[i].Name;
+        if(name != null){
+            trinventory.cells[i].innerHTML = playerInfo.inventoryitems[i].Name + ": " + playerInfo.inventoryitems[i].Amount;
+            trinventory.cells[i].title = "Attack bonus: " + playerInfo.inventoryitems[i].Attack +
+            "\nDefence bonus: " + playerInfo.inventoryitems[i].Defence +
+            "\nStrength bonus: " + playerInfo.inventoryitems[i].Strength;
+        }
+        else{
+            trinventory.cells[i].innerHTML = "&nbsp;";
+        }
+    }
+    trequipment = document.getElementById("Equipment");
+
+    for(let i = 0; i < playerInfo.equipmentitems.length; i++){
+        if(playerInfo.equipmentitems[i].Name != null){
+            trequipment.cells[i].innerHTML = playerInfo.equipmentitems[i].Name;
+            trequipment.cells[i].title = "Attack bonus: " + playerInfo.equipmentitems[i].Attack +
+            "\nDefence bonus: " + playerInfo.equipmentitems[i].Defence +
+            "\nStrength bonus: " + playerInfo.equipmentitems[i].Strength;
+        }
+    }
+}
